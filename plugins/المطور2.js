45ee03e27438b0f9b8d2794df4709ff36c4c937c
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-16 11:55:16 +0100

    Create المطور2.js

---
+++ plugins/المطور2.js
@@ -0,0 +1,39 @@
+import { createHash } from 'crypto'
+const handler = async (m, { conn, usedPrefix: _p }) => {
+  try {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+const user = global.db.data.users[m.sender];
+const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+    const text = `
+*⚘اهلا و سهلا بك يا 『${taguser}』*
+
+*↯رقم المطور:『 https://wa.me/message/RYOUR2E5SRFOL1 』*
+
+*↯انستا المطور:『 https://www.instagram.com/gojosaturo_876?igsh=amxrYmMydTh0NDN3 』*
+`.trim();
+
+    conn.sendMessage(m.chat, {
+      text,
+      contextInfo: {
+        externalAdReply: {
+          title: 'ᥡᥙᥒᥲ┇ᑲ᥆𝗍',
+          body: "البوت الخاص بمملكة SPACE🚀",
+          thumbnailUrl:'https://telegra.ph/file/7116c6426e496c317d83e.jpg',
+          sourceUrl: 'https://www.instagram.com/gojosaturo_876?igsh=amxrYmMydTh0NDN3',
+          mediaType: 2,
+        }
+      }
+    }, { quoted: m });
+  } catch (e) {
+    conn.reply(m.chat, '❎ هناك خطأ', m);
+    throw e;
+  }
+};
+
+handler.help = ['استمارة'];
+handler.tags = ['infobot'];
+handler.command = ['معلومات_المطور'];
+handler.register = false;
+
+export default handler;
