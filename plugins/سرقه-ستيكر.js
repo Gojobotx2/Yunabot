62a3c4598383a6f5534433e1668a8ab13aeb4764
Gojobotx2 <162726553+Gojobotx2@users.noreply.github.com>
2024-03-16 13:35:38 +0100

    Update سرقه-ستيكر.js

--- plugins/سرقه-ستيكر.js
+++ plugins/سرقه-ستيكر.js
@@ -1,23 +1,27 @@
 import { addExif } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
-let stiker = false
-try {
-let [packname, ...author] = text.split('|')
-author = (author || []).join('|')
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
-let img = await m.quoted.download()
-if (!img) throw '*امنح الملصق الذي تريده لإضافة حزمة واسم*'
-stiker = await addExif(img, packname || '', author || '')
-} catch (e) {
-console.error(e)
-if (Buffer.isBuffer(e)) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw '*آسف ، شيء ما خاطئ ..تحقق من أنك استجابت لملصق وأضفت اسم حزمة واسم حزمة*'
-}}
-handler.help = ['wm <packname>|<author>']
-handler.tags = ['sticker']
-handler.command = /^سرقه|سرقة$/i
+    if (!m.quoted) throw 'الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+    let stiker = false
+    try {
+        let [packname, ...author] = text.split('|')
+        author = (author || []).join('|')
+        let mime = m.quoted.mimetype || ''
+        if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+        let img = await m.quoted.download()
+        if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال : \n *.سرقه gojo bot*'
+        stiker = await addExif(img, packname || global.packname, author || global.author.replace("bobiza", ""))
+    } catch (e) {
+        console.error(e)
+        if (Buffer.isBuffer(e)) stiker = e
+    } finally {
+        if (stiker) conn.sendFile(m.chat, stiker, 'سرقه.webp', '', m, false, { asSticker: true })
+        else throw '*راسل صاحب البوت هناك مشكلة\n instagram.com/noureddine_ouafy*'
+    }
+}
+
+handler.help = ['سرقه']
+handler.tags = ['سرقه','سرقة']
+handler.command = /^take|سرقه$/i
+
 export default handler
